e909800cf88f61711253582c92bc7e90f75467f3
zaoweiceng <zaoweiceng@github,com>
2020-05-22 08:37:51 +0800

    update .c

--- helloworld.c
+++ helloworld.c
@@ -7,6 +7,8 @@ int main(void) {
 	if  (fp = fopen("d/abc.txt", "r")) {
 		fscanf(fp, "%s", buf);
 		printf("%s", buf);
+	}else{
+		printf("can't find the file");
 	}
 	printf("\nThis is the end;");
 	return 0;
